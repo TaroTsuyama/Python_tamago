4a54ffa3b2253b293d88079ce303c8e200eb7165
TaroTsuyama <51371377+TaroTsuyama@users.noreply.github.com>
2020-08-27 22:48:53 +0900

    Add files via upload

---
+++ maze/sample/maze_easy_sample.py
@@ -0,0 +1,38 @@
+import turtle
+
+# ==============================================================================
+# 設定
+# ==============================================================================
+turtle.setup(width=650,height=650)
+turtle.shape("turtle")
+turtle.speed(1)
+turtle.color("green")
+turtle.width(3)
+turtle.bgpic("maze-easy.png")
+
+
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(200)
+turtle.left(90)
+turtle.forward(150)
+turtle.right(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(150)
+turtle.left(90)
+turtle.forward(400)
+turtle.left(90)
+turtle.forward(400)
+turtle.left(90)
+turtle.forward(400)
+turtle.left(90)
+turtle.forward(100)
+
+
+input("終了するにはENTERキーを押してください")
